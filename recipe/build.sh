5d2af06708e6d5fb917cd059a5b6d02f16c34fc5
Matthias Diener <mdiener@illinois.edu>
2023-02-09 12:37:33 -0600

    add dep

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,16 @@ set -ex
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* config/
 
+if [[ $target_platform == linux-* ]]; then
+    EXTRA_ARGS=--enable-vfs-undelfs
+fi
+
 ./configure                 \
     --prefix=$PREFIX        \
     --disable-debug         \
     --without-x             \
     --with-screen=slang     \
-    --enable-vfs-undelfs    \
+    $EXTRA_ARGS             \
     --enable-vfs-sftp || { cat config.log; exit 1; }
 
 make -j$CPU_COUNT
